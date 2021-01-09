1626654a6d618a02fa5fdc29a974ffac0b8b9f44
Zhongyu Chen <34185896+zhongyuchen@users.noreply.github.com>
2021-01-09 15:19:49 +0800

    add snn_mlp_1_non_zero

--- snn_mlp_1_non_zero.py
+++ snn_mlp_1_non_zero.py
@@ -40,7 +40,7 @@ else:
 
 # arg parser
 parser = argparse.ArgumentParser(description='mlp snn')
-parser.add_argument('--config_file', type=str, default='snn_mlp_1.yaml',
+parser.add_argument('--config_file', type=str, default='snn_mlp_1_non_zero.yaml',
                     help='path to configuration file')
 parser.add_argument('--train', action='store_true',
                     help='train model')
